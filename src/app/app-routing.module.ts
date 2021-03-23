837fba8ced3d40c67c56d83ef6e52481791ab251
Jason Neal <jasonneal1@quickenloans.com>
2021-03-22 23:25:14 -0400

    ng add ng-micro-frontend --project shell --type shell --port 4200

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RouteFactory } from 'ng-micro-frontend';
 
-const routes: Routes = [];
+const routes: Routes = [
+  RouteFactory.createRoute('mfe1', 'http://localhost:4210'),
+  RouteFactory.createRoute('mfe2', 'http://localhost:4220'),
+  RouteFactory.createRoute('mfe3', 'http://localhost:4230'),
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
