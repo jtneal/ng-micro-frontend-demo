e6787ced8241776b09ab99aafaa181e01dcd80ab
Jason Neal <jasonneal1@quickenloans.com>
2021-03-22 23:25:06 -0400

    ng add ng-micro-frontend --project mfe2 --type micro --port 4220

--- projects/mfe2/src/app/app-routing.module.ts
+++ projects/mfe2/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NullComponent } from 'ng-micro-frontend';
 
-const routes: Routes = [];
+const routes: Routes = [{ component: NullComponent, path: '**' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
